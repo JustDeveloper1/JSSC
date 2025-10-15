97870c13c2193d7b5f90b8f4ba300e3d086f9d82
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-10-15 16:39:50 +0500

    Update test.js

--- test/node/test.js
+++ test/node/test.js
@@ -2,7 +2,7 @@ const {compress, decompress} = require('../../index.js');
 
 const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 const compressed = compress(text);
-const decompressed = decompress(text);
+const decompressed = decompress(compressed);
 
 if (decompressed != text) throw new Error('Failed to decompress. decompress(text) != text');
 
