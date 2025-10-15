34a560f71dafcd21fc8399d3873537d5bb14ed3b
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-10-15 16:33:30 +0500

    Create test.js

---
+++ test/node/test.js
@@ -0,0 +1,20 @@
+const {compress, decompress} = require('../../index.js');
+
+const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+const compressed = compress(text);
+const decompressed = decompress(text);
+
+if (decompressed != text) throw new Error('Failed to decompress. decompress(text) != text');
+
+const saved = (text.length - compressed.length) * 16;
+const tbits = text.length * 16;
+const cbits = compressed.length * 16;
+const dbits = decompressed.length * 16;
+
+const lines = [
+    `Saved ${saved / 8} bytes.`, '',
+    `Text         (${tbits / 8} bytes)           :   ${text}`,
+    `Compressed   (${cbits / 8} bytes)           :   ${compressed.padStart(decompressed.length, ' ')}`,
+    `Decompressed (${dbits / 8} bytes)           :   ${decompressed}`,
+]; lines[0] = lines[0].padStart(Math.floor(lines[2].length / 2));
+for (const line of lines) console.log(line);
