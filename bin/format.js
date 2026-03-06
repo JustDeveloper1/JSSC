1280ab6240e6aa82ad709bb9e41d9d10bfd0b13f
JustDeveloper <176615419+justdevw@users.noreply.github.com>
2026-03-06 22:17:19 +0500

    Update format.js

--- bin/format.js
+++ bin/format.js
@@ -4,7 +4,7 @@ import { SemVer, gt } from "semver";
 import { concat } from 'uint8arrays/concat';
 import crc32 from 'crc-32';
 import { convertBase } from "../lib/third-party/convertBase.js";
-import { stringChunks } from "../lib/utils.js";
+import { stringChunks, binToDec, decToBin } from "../lib/utils.js";
 import JUSTC from "justc";
 
 function int8(n, le = true) {
@@ -64,37 +64,15 @@ function int32(n, le = true) {
     return new Uint8Array(buffer);
 }
 
+function int16(n, le = true) {
+    const buffer = new ArrayBuffer(2);
+    const view = new DataView(buffer);
+    view.setInt16(0, n, le);
+    return new Uint8Array(buffer);
+}
+
 function data(uint8) {
-    return concat([int32(uint8.length), uint8]);
-}
-
-export async function toFile(isDir, extn, files, dirs) {
-    const header = concat([fileprefix, version(), int8(isDir ? 1 : 0)]);
-
-    const extn8 = extn == null ? data(int8(0)) : data(B64toUI8A(extn));
-
-    const fsObjE = Object.entries(files);
-    const output = [];
-    for (const [path, content] of fsObjE) {
-        const path8 = fsObjE.length == 1 ? int32(0) : data(B64toUI8A(path));
-        const file8 = data(B64toUI8A(content));
-
-        output.push(path8, file8);
-    }
-    for (let i = 0; i < dirs.length; i++) {
-        const dir8 = data(B64toUI8A(dirs[i]));
-        const zero = int32(0);
-
-        output.push(dir8, zero);
-    }
-
-    const checksum = int32(crc32.str(await JUSTC.stringify(files)));
-
-    return concat([header, extn8, checksum, ...output]);
-}
-
-function makeSemVer([major, minor, patch]) {
-    return new SemVer(major + '.' + minor + '.' + patch);
+    return [int32(uint8.length), uint8];
 }
 
 function fromInt32(bytes, le = true) {
@@ -102,8 +80,80 @@ function fromInt32(bytes, le = true) {
     return view.getInt32(0, le);
 }
 
-function corrupted() {
-    throw new Error(prefix+'Input file was corrupted.');
+function fromInt16(bytes, le = true) {
+    const view = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+    return view.getInt16(0, le);
+}
+
+export async function toFile(isDir, extn, files, dirs) {
+    const header = concat([fileprefix, version()]);
+
+    const extn8 = concat(extn == null ? data(int8(0)) : data(B64toUI8A(extn)));
+
+    const fsObjE = Object.entries(files);
+    const noPath = fsObjE.length == 1;
+    const output = [];
+    const files8 = [];
+    let need = 1;
+    function set(num) {
+        let n = 4;                      // int32
+        if (num <= 0xFF) n = 1;         // int8
+        else if (num <= 0xFFFF) n = 2;  // int16
+        need = Math.max(need, n);
+    }
+    for (const [path, content] of fsObjE) {
+        const path8 = noPath ? [int32(0)] : data(B64toUI8A(path));
+        const file8 = data(B64toUI8A(content));
+
+        files8.push(path8, file8);
+        set(fromInt32(path8[0]));
+        set(fromInt32(file8[0]));
+    }
+
+    const func = need == 1 ? int8 : need == 2 ? int16 : int32;
+    function num(int32) {
+        return func(fromInt32(int32));
+    }
+    for (let i = 0; i < files8.length; i++) {
+        const [length, data] = files8[i];
+        output.push(num(length));
+        if (typeof data !== 'undefined') output.push(data);
+    }
+
+    for (let i = 0; i < dirs.length; i++) {
+        const dir8 = concat(data(B64toUI8A(dirs[i])));
+        const zero = func(0);
+
+        output.push(dir8, zero);
+    }
+
+    const npfiles = {};
+    npfiles[name__] = fsObjE[0][1];
+    const checksum = int32(crc32.str(await JUSTC.stringify(
+        noPath ? npfiles : files
+    )));
+
+    const bindata = [
+        decToBin(need - 1, 2), isDir ? '1' : '0'
+    ].join('');
+
+    return concat([
+        header,
+        int8(binToDec(bindata)),
+        extn8, 
+        checksum, 
+        ...output
+    ]);
+}
+
+function makeSemVer([major, minor, patch]) {
+    return new SemVer(major + '.' + minor + '.' + patch);
+}
+
+function corrupted(extra) {
+    throw new Error(prefix+'Input file was corrupted.'+(
+        extra ? ' ('+extra+')' : ''
+    ));
 }
 
 export async function fromFile(uint8) {
@@ -117,15 +167,20 @@ export async function fromFile(uint8) {
         semver.major, semver.minor, semver.patch
     ]))) throw new Error(prefix+`Input file was compressed with a higher ${name__} version.`);
 
-    const isDir = uint8[8] == 1;
+    const dataBin = decToBin(uint8[8]);
+    let need = binToDec(dataBin.slice(0,-1)) + 1;
+    const isDir = dataBin.slice(-1) == '1';
+    if (isNaN(need)) need = 1;
     
     let i = 9;
+    let legnthLength = 4;
+    let func = fromInt32;
     function read() {
-        if (i > uint8.length) return null;
-        if (i + 4 > uint8.length) corrupted();
+        if (i >= uint8.length) return null;
+        if (i + legnthLength > uint8.length) corrupted();
 
-        const length = fromInt32(uint8.subarray(i, i + 4));
-        i += 4;
+        const length = func(uint8.subarray(i, i + legnthLength));
+        i += legnthLength;
 
         if (length == 0) return 0;
 
@@ -140,6 +195,10 @@ export async function fromFile(uint8) {
     const extn = read();
     const checksum = fromInt32(uint8.subarray(i, i + 4));
     i += 4;
+    legnthLength = need;
+    func = need == 1 ? function(ui8a) {
+        return ui8a[0];
+    } : need == 2 ? fromInt16 : fromInt32;
 
     const files = {};
     const dirs = [];
@@ -157,7 +216,7 @@ export async function fromFile(uint8) {
     }
 
     if (checksum != crc32.str(await JUSTC.stringify(files))) {
-        //corrupted();
+        corrupted('crc32');
     }
 
     return {
