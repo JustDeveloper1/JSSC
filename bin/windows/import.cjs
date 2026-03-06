0e696255cca07e32d6fa15dba7a52567582ed201
JustDeveloper <176615419+justdevw@users.noreply.github.com>
2026-03-06 22:47:33 +0500

    Fix

--- bin/windows/import.cjs
+++ bin/windows/import.cjs
@@ -1,12 +1,12 @@
 if (process.platform !== "win32") module.exports = {
     compress: () => {},
     message : () => {}
-};
+}; else {
+    const { compress } = require("./ui");
+    const { message } = require("./message");
 
-const { compress } = require("./ui");
-const { message } = require("./message");
-
-module.exports = {
-    compress,
-    message
-};
+    module.exports = {
+        compress,
+        message
+    };
+}
