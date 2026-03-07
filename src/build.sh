6f6dec8a7b4322654860d42ee6d4df202a2836af
JustDeveloper <176615419+justdevw@users.noreply.github.com>
2026-03-07 12:21:41 +0500

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -7,9 +7,10 @@ rm -f src/emoji.js
 npx rollup -c
 
 reserved="reserved=['compress','decompress','JSSC','jssc','compressLarge','compressToBase64','compressLargeToBase64','decompressFromBase64','cache','version','JSSC1']"
-npx terser dist/jssc.js -c -m "$reserved" --format "ascii_only=true" -o dist/index.min.js
+npx terser dist/jssc.js -c -m "$reserved" --format "ascii_only=true" -o dist/jssc.min.js
 npx terser dist/worker.js -c -m "$reserved" --module --format "ascii_only=true" -o dist/worker.min.js
 cp dist/jssc.js index.js
+cp dist/jssc.min.js index.min.js
 cp dist/worker.js worker.js
 cp dist/worker.min.js worker.min.js
 cp src/index.d.ts dist/jssc.d.ts
